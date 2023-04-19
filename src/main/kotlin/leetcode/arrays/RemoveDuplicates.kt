f2061750b9eb921e10dc1a6ed2b42a7041846551
MrPanlong <66047085+MrPanlong@users.noreply.github.com>
2023-04-19 23:24:19 +0800

    the sixth commit

--- src/main/kotlin/leetcode/arrays/RemoveDuplicates.kt
+++ src/main/kotlin/leetcode/arrays/RemoveDuplicates.kt
@@ -1,2 +1,17 @@
 package leetcode.arrays
 
+fun removeDuplicates(s:String):String{
+    var stack = StringBuilder()
+    var top = -1
+    for (i in 0 until s.length) {
+        var ch = s[i]
+        if (top >= 0 && stack[top] == ch){
+            stack.deleteCharAt(top)
+            --top
+        }else{
+            stack.append(ch)
+            ++top
+        }
+    }
+    return stack.toString()
+}
